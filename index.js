a31f7b736a736a3cae89ad7f90d02360f821850c
Aluoch111 <grace.aluoch@student.moringaschool.com>
2022-08-15 19:11:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    employeeName : "Sam",
+    streetAddress : "12 hyde court",
+};
+function updateEmployeeWithKeyAndValue(employee,streetAddress){
+    const newEmployee = {...employee};
+    newEmployee [streetAddress] = "11 Broadway";
+    return newEmployee ;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee){
+    employee.streetAddress = "12 Broadway";
+    return employee ;
+}
+function deleteFromEmployeeByKey(employee,employeeName){
+    const mutEmployee = {...employee};
+     delete(mutEmployee[employeeName]);
+    return mutEmployee ;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,employeeName){
+   delete (employee[employeeName]);
+    return employee ;
+}
